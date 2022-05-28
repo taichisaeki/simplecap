f250b78f3743bed50a9e1f1509ac19d073ede607
taitai0831 <33798018+taitai0831@users.noreply.github.com>
2022-05-28 11:06:20 +0900

    ファイルをアップロード

---
+++ script.js
@@ -0,0 +1,61 @@
+let datalist = {};
+localStorage.setItem("datalist", JSON.stringify(datalist));
+
+function vr_function() {
+    window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = 'ja';
+    recognition.interimResults = true;
+    recognition.maxAlternatives = 10;
+    var last_finished = '';
+    let current_transcripts = '';
+    let transcript = '';
+
+    var date = new Date() ;
+    var timestamp = date.getTime() ;
+    var span = document.createElement("span");
+    span.setAttribute("id", timestamp);
+
+
+    recognition.onresult = function(event) {
+        var results = event.results;
+
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+            transcript = event.results[i][0].transcript;
+            
+            if (event.results[i].isFinal) {
+                last_finished = results[i][0].transcript;
+                var result_log = last_finished + "。" + '<br>';
+                document.getElementById('result_text').appendChild(span);
+                document.getElementById(timestamp).insertAdjacentHTML('beforeend',result_log);
+                vr_function();
+            } else {
+                current_transcripts += results[i][0].transcript;
+                document.getElementById('innter_text').innerHTML = '<span style="color: darkgray;">' + results[i][0].transcript + '</span>';
+                flag_speech = 1;
+            }
+            }
+        }
+
+       
+          
+          
+
+            //document.getElementById("result_text").innerHTML = '<i style="color:#ddd;">' + interimTranscript + '</i>' + '<br>';
+        //$("#result_text").empty();
+        /*for (var i = event.resultIndex; i<results.length; i++){
+            if(results[i].isFinal){
+                for (var j = 0; j<recognition.maxAlternatives; j++){
+                    $("#result_text").append("<p>" + "ランク" + j + " " + results[i][j].transcript +
+                                                    ": " + results[i][j].confidence +
+                                                "</p>");
+                }
+            }
+        }*/
+
+
+
+    flag_speech = 0;
+    //要検討 document.getElementById('stbtn').textContent = "認識中・・";
+    recognition.start();
+}
